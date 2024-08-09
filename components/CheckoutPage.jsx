3c7af9881f281cf6db3b8770fc24f01348ac8758
mehdi hou <mehdi5juillet@gmail.com>
2024-08-09 16:52:38 +0200

    change url

--- components/CheckoutPage.jsx
+++ components/CheckoutPage.jsx
@@ -57,7 +57,7 @@ const CheckoutPage = ({amount}) => {
         clientSecret,
         confirmParams: {
 
-            return_url: `http://localhost:3000/payment-success?amount=${amount}`
+            return_url: `https://stripe-test-nu.vercel.app/payment-success?amount=${amount}`
           
         },
       });
